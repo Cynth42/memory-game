c4a81d9c1f5cd6baa549ba16aaa5e76270dae805
Cynth42 <cclinc2005@yahoo.com>
2018-11-17 23:27:17 -0500

    Modify the files

--- js/app.js
+++ js/app.js
@@ -264,6 +264,7 @@ function gameOver() {
   if(matchCard.length == totalCards) {
     clearInterval(interval);
     gameOverMessage();
+    confettiAnimation();
  }
 }
 
@@ -297,9 +298,11 @@ function stopTimer() {
  */
 function closeModal() {
   const closeIcon = document.querySelector(".exit");
-  closeIcon.addEventListener("click", function(DeactivateConfetti){
+  closeIcon.addEventListener("click", function(){
   const popUp = document.getElementById("modal");
   popUp.classList.remove("show");
+  document.querySelector("#canvas").style.visibility="hidden";
+
   //initGame();
 })
 }
@@ -310,6 +313,7 @@ function closeModal() {
  function closeButton() {
    const popUp = document.getElementById("modal");
    popUp.classList.remove("show");
+   document.querySelector("#canvas").style.visibility="hidden";
    initGame();
  }
 
@@ -319,17 +323,139 @@ function closeModal() {
 function playAgain() {
   const popUp = document.getElementById("modal");
     popUp.classList.remove("show");
+    document.querySelector("#canvas").style.visibility="hidden";
     initGame();
 }
 
 /**
  * When the user clicks anywhere outside of the
- * modal, close it
+ * modal, close it from stackoverflow
  */
 window.onclick = function(event) {
   const popUp = document.getElementById("modal");
   if (event.target == popUp) {
       popUp.classList.remove("show");
+      document.querySelector("#canvas").style.visibility="hidden";
       initGame();
     }
+}
+
+function confettiAnimation() {
+
+    document.querySelector("#canvas").style.visibility="visible";
+
+   //canvas init
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const maxConfettis = 150;
+    const particles = [];
+
+   //canvas dimensions
+    let W = window.innerWidth;
+    let H = window.innerHeight;
+    canvas.width = W;
+ 	  canvas.height = H;
+
+    const colorOptions = [
+      "DodgerBlue",
+      "OliveDrab",
+      "Gold",
+      "Pink",
+      "SlateBlue",
+      "LightBlue",
+      "Gold",
+      "Violet",
+      "PaleGreen",
+      "SteelBlue",
+      "SandyBrown",
+      "Chocolate",
+      "Crimson"
+ ];
+
+    function randomFromTo(from, to) {
+      return Math.floor(Math.random() * (to - from + 1) + from);
+    }
+
+    function initConfettiParticle() {
+
+      this.x = Math.random() * W; // x
+      this.y = Math.random() * H - H; // y
+      this.r = randomFromTo(11, 33); // radius
+      this.d = Math.random() * maxConfettis + 11; //density
+      this.color =
+      colorOptions[Math.floor(Math.random() * colorOptions.length)];
+      this.tilt = Math.floor(Math.random() * 33) - 11;
+      this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+      this.tiltAngle = 0;
+
+      this.draw = function() {
+        context.beginPath();
+        context.lineWidth = this.r / 2;
+        context.strokeStyle = this.color;
+        context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+        context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+       return context.stroke();
+     };
+   }
+
+   // Draws the flakes
+   function draw() {
+     const results = [];
+
+    //Set recursive frame
+     requestAnimationFrame(draw);
+
+     context.clearRect(0, 0, W, window.innerHeight);
+
+     for (var i = 0; i < maxConfettis; i++) {
+       results.push(particles[i].draw());
+     }
+
+     let particle = {};
+     let remainingFlakes = 0;
+     for (var i = 0; i < maxConfettis; i++) {
+       particle = particles[i];
+
+       particle.tiltAngle += particle.tiltAngleIncremental;
+       particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+       particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+     if (particle.y <= H)
+       remainingFlakes++;
+
+     /**
+      * If a confetti has fluttered out of view,
+      * bring it back to above the viewport and let if
+      * re-fall.
+      */
+       if (particle.x > W + 30 || particle.x < -30 ||
+       particle.y > H) {
+       particle.x = Math.random() * W;
+       particle.y = -30;
+       particle.tilt = Math.floor(Math.random() * 10) - 20;
+     }
+   }
+   return results;
+ }
+
+   window.addEventListener (
+     "resize",
+     function() {
+       W = window.innerWidth;
+       H = window.innerHeight;
+       canvas.width = window.innerWidth;
+       canvas.height = window.innerHeight;
+     },
+     false
+   );
+
+   // Push new confetti objects to `particles[]`
+     for (var i = 0; i < maxConfettis; i++) {
+       particles.push(new initConfettiParticle());
+     }
+
+   // Initialize canvas
+     canvas.width = W;
+     canvas.height = H;
+     draw();
 }
